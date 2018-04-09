c5ec98b975960eef6830889d9f710ec08c9cf8f5
L-eeX <1035868977@qq.com>
2018-04-09 17:41:08 +0800

    text

---
+++ main.ts
@@ -0,0 +1,19 @@
+    /**
+     * Provides access to basic micro:bit functionality.
+     */
+    //% color=#0078D7 weight=100 icon="\uf00a"
+namespace test {
+
+    /**
+     * Disconnect the serial port.
+    */
+    //% weight=97
+    //% blockGap=50
+    //% blockId=showLedss
+    //% block="showLedss"
+    //% shim=test::showLedss
+    export function showLedss(leds: string, interval?: number): void {
+        
+    }
+    
+}
---
+++ ttt.cpp
@@ -0,0 +1,25 @@
+#include "pxt.h"
+
+
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=#0078D7 weight=100 icon="\uf00a"
+namespace test {
+
+    /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help=basic/show-leds
+    //% weight=95 blockGap=8
+    //% imageLiteral=1 async
+    //% blockId=showLedss
+    //% block="show leds" icon="\uf00a"
+    //% parts="ledmatrix"
+    void showLedss(ImageLiteral leds, int interval = 400) {
+      uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, interval);
+    }
+
+}
